f0627a397019fc732b3c43456db74c81b9c4648a
Perplexed Coffee <perplexedcoffee@gmail.com>
2021-03-27 03:10:56 -0700

    first commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * A C program that prints the size of various types on the computer it is compiled and run on
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char: %c byte(s)\n", sizeof(c));
+	printf("Size of an int: %d byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %lld byte(s)\n", sizeof(lli));
+	printf("Size of a float: %f byte(s)\n", sizeof(f));
+	return (0);
+}
